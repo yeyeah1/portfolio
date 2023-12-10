c095081d50407d877973976a166c509b143c88a1
2_yewon <2_yewon@naver.com>
2023-12-11 00:27:20 +0900

    commit

--- index.js
+++ index.js
@@ -14,6 +14,26 @@ window.addEventListener(
   false
 );
 
+//모달
+const $modalBg = document.getElementsByClassName("modal-background");
+const $btnOpen = document.getElementsByClassName("btn-open");
+const $btnClose = document.getElementsByClassName("btn-close");
+
+function modal(num) {
+  $btnOpen[num].addEventListener("click", () => {
+    $modalBg[num].style.display = "flex";
+    document.body.style.overflow = "hidden";
+  });
+  $btnClose[num].addEventListener("click", () => {
+    $modalBg[num].style.display = "none";
+    document.body.style.overflow = "unset";
+  });
+}
+
+for (let i = 0; i < $btnOpen.length; i++) {
+  modal(i);
+}
+
 //moveTop btn
 let moveToTop = function () {
   document.body.scrollIntoView({ behavior: "smooth" });
