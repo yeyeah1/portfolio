99b2b76c8acb5419a144a48f765062031701b588
2_yewon <2_yewon@naver.com>
2023-12-10 22:57:46 +0900

    commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//스크롤바
+let scrollTop = 0;
+let bar = document.getElementsByClassName("bar-ing")[0];
+
+window.addEventListener(
+  "scroll",
+  () => {
+    scrollTop = document.documentElement.scrollTop;
+    let per = Math.ceil(
+      (scrollTop / (document.body.scrollHeight - window.outerHeight)) * 100
+    );
+    bar.style.width = per + "%";
+  },
+  false
+);
+
+//moveTop btn
+let moveToTop = function () {
+  document.body.scrollIntoView({ behavior: "smooth" });
+};
